7b276293c9e3e32267d903efb5a18e624e549b88
Raphael Etim <raphaelumoh@gmail.com>
2020-04-09 09:11:57 +0100

    add middleware for the root route

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors')
 const os = require('os');
+const transactions = require('./routes/transactions');
 
 const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.use('/api/v1/transactions', transactions);
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
