d58b838ca489323a254d7cd4e2d874ffbea9b015
Raphael Etim <raphaelumoh@gmail.com>
2020-04-23 16:32:51 +0100

    remove logger and redux-dev-tool

--- src/client/store/store.js
+++ src/client/store/store.js
@@ -1,12 +1,11 @@
 import { createStore, compose, applyMiddleware, } from 'redux';
-import { createLogger } from 'redux-logger';
+
 import thunk from 'redux-thunk'
 import { transactionReducer, initialState } from '../reducer/transactionReducer'
 
-const logger = createLogger();
+
 const store = createStore(transactionReducer, initialState, compose(
-  applyMiddleware(logger, thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk),
 ));
 
 export default store;
